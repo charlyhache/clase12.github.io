19437303a6a0079e85447987f0f7b596202028f4
charlyhache <167575346+charlyhache@users.noreply.github.com>
2024-05-26 21:28:32 -0300

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,15 @@
+
+var nombre = prompt("ingrese su nombre")
+if(nombre != ""){
+    saludo(nombre);
+
+}else{
+    alert ("Por favor...ingresa tu nombre")
+
+} 
+   
+   
+function saludo(nombre){
+   alert("bienvenido " + nombre)
+    }
+   
